aa5c0898d81a13ecd4a00bb88c6f316ee26f9f0b
Mony <mmilancastro491@gmail.com>
2025-06-19 04:21:50 +0200

    documentacion

---
+++ docs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"controladores"},{"l":"modelo"},{"l":"modelo.entidades"},{"l":"vista"},{"l":"vista.componentes"}];updateSearchResults();
